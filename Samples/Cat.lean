264a68b0e9f7aaf2a83bc0cb61e6a65e34923b3e
Ry <ry.diffusion@proton.me>
2023-10-19 14:31:10 -0300

    feat: Buffer API

--- Samples/Cat.lean
+++ Samples/Cat.lean
@@ -3,6 +3,9 @@ import «Luva»
 def appMain (path: String) : IO UInt32 := do
   let req ← FileSystem.Request.mk
   let loop ← Loop.mk
+  let buff ← Buffer.mk 1024
+  let byteArray ← buff.asByteArray()
+  ← IO.println s!"buffer {byteArray}"
   return ← loop.run Loop.RunMode.once
 
 def main (args: List String) : IO UInt32 := match (args) with
